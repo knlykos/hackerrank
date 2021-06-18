c1d67e87d449ff379a20d900ea8f91de515388d5
Nefi Lopez Garcia <nefi.lopezg@gmail.com>
2021-06-17 20:12:29 -0700

    resolving new years chaos

--- arrays/new-year-chaos.ts
+++ arrays/new-year-chaos.ts
@@ -1,34 +1,61 @@
+
 function minimumBribes(q: number[]) {
-  debugger;
-  let substractRes = 0;
-  let indexToMove = 0;
-  let numberToMove = 0;
-  for (let x = 0; x < q.length;x++ ) {
-    console.log(q[x]);
-    // console.log(q[x] - q[x + 1])
-    substractRes = q[x] - q[x + 1];
-    numberToMove = q[x];
-    console.log(numberToMove," numberToMove")
-    if (substractRes > 0) {
-      indexToMove = x + substractRes;
-      console.log(numberToMove, "numberToMove");
-      console.log(x, "x,", q[x], "q[x]");
-      console.log(indexToMove, "indexToMove");
-      for (let y = x; y <= substractRes; y++) {
-    
-        if (substractRes === y) {
-          q[y] = numberToMove;
-        } else {
-          q[y] = q[y + 1]
-        }
+  let brides = 0;
+  let bridesByElement: number[] = [];
+  let brideByElement = 0;
+  let x = 0;
+  let difference = 0;
+  let index2Check = 0;
+  let verifying = false;
+  let completed = false;
+  
+  while (completed === false) {
+    // debugger
+    // console.log(originalPosition);
+    // difference = q[x] - q[x - 1 === -1 ? 0 : x - 1];
+    // result = q[x] > q[x - 1 === -1 ? 0 : x - 1];
+    difference = q[x] - x + 1
+    if (verifying === false) {
+
+      if (difference !== 0) {
+        verifying = true;
+        index2Check = x + 1;
+      } else {
+        x++;
       }
-      x = 0;
-
-
+    } else if (verifying === true) {
+      if (q[x] > q[index2Check] === true) {
+        brides++;
+        brideByElement++
+      }
+      index2Check++;
+      
+      if (index2Check >= q.length) {
+        verifying = false;
+        index2Check = 0;
+        bridesByElement.push(brideByElement);
+        brideByElement = 0;
+        x++;
+        
+      }
+  
+    }
+    if (x >= q.length) {
+      completed = true;
+      
     }
   }
-}
+  // console.log(bridesByElement);
+  if (brideByElement > 2) {
+    console.log("Too chaotic")
+  } else {
+    console.log(brides)
 
-const exerciseOne = [5, 2, 1, 4, 3];
+  }
+  // console.log(brides, "brides");
+
+}
+// [ 2, 1, 5, 3, 4 ]
+
+const exerciseOne = [2, 5, 1, 3, 4];
 const res = minimumBribes(exerciseOne);
-console.log(res);
